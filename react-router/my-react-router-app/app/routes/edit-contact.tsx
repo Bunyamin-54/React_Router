a907a19e65dda7015ccecf567ae9b33ff1cb701e
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-06-22 13:57:41 +0200

    useNavigate functions

--- react-router/my-react-router-app/app/routes/edit-contact.tsx
+++ react-router/my-react-router-app/app/routes/edit-contact.tsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router";
+import { Form, redirect, useNavigate, useNavigation } from "react-router";
 import type { Route } from "./+types/edit-contact";
 
 import { getContact, updateContact } from "../data";
@@ -19,6 +19,8 @@ export async function loader({ params }: Route.LoaderArgs) {
  
 export default function EditContact({ loaderData }: Route.ComponentProps) {
   const { contact } = loaderData;
+
+  const navigate = useNavigate();
 
   return (
     <Form key={contact.id} id="contact-form" method="post">
@@ -64,7 +66,7 @@ export default function EditContact({ loaderData }: Route.ComponentProps) {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button" onClick={() => {navigate(-1)}}>Cancel</button>
       </p>
     </Form>
   );
