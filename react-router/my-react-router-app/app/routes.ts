fedebaf703a1529dc17fab8873756bb5a1f4d4d1
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-06-22 10:20:29 +0200

    configure all our routes.

--- react-router/my-react-router-app/app/routes.ts
+++ react-router/my-react-router-app/app/routes.ts
@@ -1,3 +1,10 @@
 import type { RouteConfig } from "@react-router/dev/routes";
 
-export default [] satisfies RouteConfig;
+import {route} from "@react-router/dev/routes"
+
+export default [
+
+    route ("contacts/:contactId" , "routes/contact.tsx")
+
+
+] satisfies RouteConfig;
