7ce1e1e9df73b8cd108a6b1ce0bf625d1dd50694
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-06-22 13:38:26 +0200

    action for formData

--- react-router/my-react-router-app/app/routes/edit-contact.tsx
+++ react-router/my-react-router-app/app/routes/edit-contact.tsx
@@ -1,7 +1,7 @@
-import { Form } from "react-router";
+import { Form, redirect } from "react-router";
 import type { Route } from "./+types/edit-contact";
 
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const contact = await getContact(params.contactId);
@@ -10,7 +10,13 @@ export async function loader({ params }: Route.LoaderArgs) {
   }
   return { contact };
 }
-
+ export async function action ({ request, params }: Route.ActionArgs) {
+   const formData = await request.formData();
+   const updates = Object.fromEntries(formData);
+   await updateContact(params.contactId, updates);
+   return redirect(`/contacts/${params.contactId}`);
+ }
+ 
 export default function EditContact({ loaderData }: Route.ComponentProps) {
   const { contact } = loaderData;
 
