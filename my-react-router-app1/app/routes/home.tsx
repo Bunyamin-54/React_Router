06b4d760023def279eaa568b60fcdbd64f651068
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-07-28 21:28:47 +0200

    react router app

--- my-react-router-app1/app/routes/home.tsx
+++ my-react-router-app1/app/routes/home.tsx
@@ -7,10 +7,20 @@ export function meta({}: Route.MetaArgs) {
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
-
-export default function Home() {
+export async function loader() {
+  const res = await fetch("https://6a4d62d1e1cf82a4a17e544d.mockapi.io/Blogs"); 
+  return res.json()
+}
+export default function Home({loaderData, actionData, params, matches}: Route.ComponentProps) {
   return (
     <div className="container mx-auto p-4 text-center">
+  
+        <h1>Welcome to My Route with Props!</h1>
+        <p>Loader Data: {JSON.stringify(loaderData)}</p>
+        <p>Action Data: {JSON.stringify(actionData)}</p>
+        <p>Route Parameters: {JSON.stringify(params)}</p>
+        <p>Matched Routes: {JSON.stringify(matches)}</p>
+    
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page content.</p>
       <div className="flex flex-col gap-4 mt-4">
