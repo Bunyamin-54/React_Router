618f57b746946967c9d82a4e84f8783ec2d97fa2
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-08-01 18:15:47 +0200

    home page react routes

--- my-react-router-app1/app/routes/home.tsx
+++ my-react-router-app1/app/routes/home.tsx
@@ -23,11 +23,11 @@ export default function Home({loaderData, actionData, params, matches}: Route.Co
   return (
     <div className="container mx-auto p-4 text-center">
   
-        <h1>Welcome to My Route with Props!</h1>
-        <p>Loader Data: {JSON.stringify(loaderData)}</p>
+        {/* <h1>Welcome to My Route with Props!</h1> */}
+        {/* <p>Loader Data: {JSON.stringify(loaderData)}</p>
         <p>Action Data: {JSON.stringify(actionData)}</p>
         <p>Route Parameters: {JSON.stringify(params)}</p>
-        <p>Matched Routes: {JSON.stringify(matches)}</p>
+        <p>Matched Routes: {JSON.stringify(matches)}</p> */}
     
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page content.</p>
